b0206630a43267c7513a3600fd6d93521dd2447f
Dino Pacariz <dinopacariz@gmail.com>
2020-10-17 10:14:48 +0200

    ändrat footer o header till main

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     addHeader()
     addFooter()
 })
+
 function addHeader() {
     let body = document.getElementsByTagName('body')[0]
     
