6257e2a76aabfe0995e12502da7c5532b7b6e9ab
Dino Pacariz <dinopacariz@gmail.com>
2020-10-18 21:37:04 +0200

    cleaned up the whole gode structure
    filters work, sorting works.

--- js/events.js
+++ js/events.js
@@ -1,117 +1,61 @@
-/*
-let eventTime = new Date("2020-10-22 11:11:23")
-let now = new Date()
-console.log("Event: "+eventTime)
-console.log("Now: "+now)
+let filteredCategories = []
 
-if (eventTime > now) {
-    console.log("the event has not passed")
-}
-*/
-
-/*
-console.log(Date.parse("0020-10-22 11:11:23"))
-console.log(Date.parse(Date()))
-*/
-
-document.addEventListener('DOMContentLoaded', async function(){
-    let now = new Date()
-    let pastEvents = []
-    let upcomingEvents = []
-    let upcomingEventsDiv = document.getElementById('upcomingEvents')
-    let pastEventsDiv = document.getElementById('pastEvents')
-
+document.addEventListener('DOMContentLoaded', async function(){  
     let events = await fetchEvents()
-
-    Object.keys(events).forEach(function(index) {
-        let eventDate = new Date(events[index].startDatetime)
-        if (eventDate < now) {
-            pastEvents.push(events[index])
-        }
-        else {
-            upcomingEvents.push(events[index])
-        }
-    });
-    
-    console.log(events)
-
-    
-
-    upcomingEvents.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
-    
-    addEvents(upcomingEvents,upcomingEventsDiv)
-    addEvents(pastEvents,pastEventsDiv)
-
-    let sort = document.getElementById('sort')
-
-    sort.addEventListener('change', function(){
-        let dateRadio = document.getElementById('date')
-
-        while (upcomingEventsDiv.childNodes.length) {
-            upcomingEventsDiv.removeChild(upcomingEventsDiv.lastChild);
-        }
-        while (pastEventsDiv.childNodes.length) {
-            pastEventsDiv.removeChild(pastEventsDiv.lastChild);
-        }
-
-        if (dateRadio.checked == true) {
-            upcomingEvents.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
-            pastEvents.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
-            addEvents(upcomingEvents,upcomingEventsDiv)
-            addEvents(pastEvents,pastEventsDiv)
-        }
-        else {
-            upcomingEvents.sort(function(a, b){return a.name.localeCompare(b.name)});
-            pastEvents.sort(function(a, b){return a.name.localeCompare(b.name)});
-            addEvents(upcomingEvents,upcomingEventsDiv)
-            addEvents(pastEvents,pastEventsDiv)
-        }
-    })
-
-    let eventDivs = document.getElementsByClassName('event')
-    let filter = document.getElementById('filter')
-    
-    filter.addEventListener('click', function(e){
-        if (!e.target.classList.contains('filterChoice')) {
-            return
-        }
-
-        if (!e.target.classList.contains('filtered')) {
-            e.target.classList.add('filtered')
-
-            for (let event of eventDivs) {
-                if(event.getAttribute('category') == e.target.id) {
-                    event.classList.add('hidden')
-                }
-            }
-        }
-        else {
-            e.target.classList.remove('filtered')
-            
-            for (let event of eventDivs) {
-                if(event.getAttribute('category') == e.target.id) {
-                    event.classList.remove('hidden')
-                }
-            }
-        }
-    })
-
+    updateEvents(events)
+    eventListeners(events)
 })
 
 async function fetchEvents() {
     let file = new Request('../data/events.json')
-    let output = await fetch(file)
+    let fetched = await fetch(file)
     .then(response => response.json())
     .then(data => {return data})
-    return output.events
+    return fetched.events
 }
 
-function addEvents(inputArray, outputDiv) {
-    for (let event of inputArray) {
+function updateEvents(events) {
+    removeEvents()
+    addEvents(sortEvents(events))
+}
+
+function sortEvents(events) {
+    let dateRadio = document.getElementById('date')
+    
+    if (dateRadio.checked == true) {            
+        events.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
+    }
+    else {
+        events.sort(function(a, b){return a.name.localeCompare(b.name)});
+    }
+    return events
+}
+
+function removeEvents() {
+    let upcomingEventsDiv = document.getElementById('upcomingEvents')
+    let pastEventsDiv = document.getElementById('pastEvents')
+
+    while (upcomingEventsDiv.childNodes.length) {
+        upcomingEventsDiv.removeChild(upcomingEventsDiv.lastChild);
+    }
+    while (pastEventsDiv.childNodes.length) {
+        pastEventsDiv.removeChild(pastEventsDiv.lastChild);
+    }
+}
+
+function addEvents(events) {
+    let upcomingEventsDiv = document.getElementById('upcomingEvents')
+    let pastEventsDiv = document.getElementById('pastEvents')
+    let now = new Date()
+
+    for (let event of events) {
         let outerDiv = document.createElement('div')
         outerDiv.classList.add('event')
         outerDiv.setAttribute('category', event.category)
-        
+        if (filteredCategories.includes(event.category)){
+            outerDiv.classList.add('hidden')
+        }
+
         let imageDiv = document.createElement('div')
         imageDiv.classList.add('thumbnail')
         let image = document.createElement('img')
@@ -136,6 +80,46 @@ function addEvents(inputArray, outputDiv) {
         outerDiv.appendChild(imageDiv)
         outerDiv.appendChild(infoDiv)
 
-        outputDiv.appendChild(outerDiv)
+        let eventDate = new Date(event.startDatetime)
+
+        if (eventDate > now) {
+            upcomingEventsDiv.appendChild(outerDiv)
+        } else pastEventsDiv.appendChild(outerDiv)
     }
+}
+
+function eventListeners(events) {
+    let eventDivs = document.getElementsByClassName('event')
+    let filter = document.getElementById('filter')
+    let sort = document.getElementById('sort')
+    
+    sort.addEventListener('change', function(){
+        updateEvents(events)
+    })
+
+    filter.addEventListener('click', function(e){
+        if (!e.target.classList.contains('filterChoice')) {
+            return
+        }
+        if (!e.target.classList.contains('filtered')) {
+            filteredCategories.push(e.target.id)
+            e.target.classList.add('filtered')
+
+            for (let event of eventDivs) {
+                if(event.getAttribute('category') == e.target.id) {
+                    event.classList.add('hidden')
+                }
+            }
+        }
+        else {
+            filteredCategories.splice(filteredCategories.indexOf(e.target.id), 1)
+            e.target.classList.remove('filtered')
+            
+            for (let event of eventDivs) {
+                if(event.getAttribute('category') == e.target.id) {
+                    event.classList.remove('hidden')
+                }
+            }
+        }
+    })
 }
