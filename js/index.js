315f4c7cdbefb3f63b6bc92e3decd85486967867
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-18 12:54:20 +0200

    got cycleDisplay to cycle images, set up temporary EventData-class with placeholder-images

--- js/index.js
+++ js/index.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    addDisplay();
-
-   
+    
+    eventData = new EventData()
+    cycleDisplay();
 
 })
 
-function addDisplay(){
-     //-----placeholder-----//
-     let id = 0;
-     let images = ["https://cykelrundan.se/wp-content/uploads/2017/03/Concert-Event-14-1024x684.jpg",
-                   "https://brannovardshus.se/wp-content/uploads/2016/09/julbord.jpg",
-                   "https://tolvstockholm.se/wp-content/uploads/2019/04/tolvkonferensstockholm-1218x420.jpg"]
-     //--------------------//
+function cycleDisplay(){
+    
+    
+    //console.log("hej " + eventData.id);
 
-    id++;
-    if (id > 2){
-        id = 0;
+    eventData.id++;
+    if (eventData.id > 2){
+        eventData.id = 0;
     }
-    document.getElementById("display").style.backgroundImage = "url(" + images[id] + ")";
-    setInterval("addDisplay", 2000);
+    document.getElementById("display").style.backgroundImage = "url(" + eventData.images[eventData.id] + ")";
+    setTimeout(cycleDisplay, 5000);
     
 
 
-    console.log("hej " + id);
+    //console.log("hej " + eventData.id);
+}
+
+class EventData{
+    constructor(id = 0){
+        //-----placeholder-----//
+        this.id = id;
+        this.images = ["https://cykelrundan.se/wp-content/uploads/2017/03/Concert-Event-14-1024x684.jpg",
+                      "https://brannovardshus.se/wp-content/uploads/2016/09/julbord.jpg",
+                      "https://tolvstockholm.se/wp-content/uploads/2019/04/tolvkonferensstockholm-1218x420.jpg"]
+        //--------------------//
+    }    
 }
