0d4ba48bb2d5a003b78359b501dfa159c7b90f97
rikardbjerkenius <rikard@hullo.se>
2020-10-18 14:30:56 +0200

    fetch events

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,44 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', async function(){
+
+    const eventListDiv = document.querySelector('.eventList')
+    const guestsListDiv = document.querySelector('.guestsList')
+    const adminsListDiv = document.querySelector('.adminsList')
+
+    let now = new Date()
+    let guests = []
+    let admins = []
+
+    let events = await fetchEvents()
+
+    //showEvents(events)
 
 })
 
-
-var allEvents = fetch("../data/events.json")
+async function fetchEvents() {
+    await fetch("../data/events.json")
     .then(response => response.json())
     .then(json => {
+        console.log(json.events)
         return json.events
     });
+}
 
-console.log(allEvents)
+function showEvents(events){
+    events.forEach(event => {
+        let div =  document.createElement('div')
+        div.textContent = event.name
+        eventListDiv.appendChild(div)
+    })
+}
+
+function showEvent(eventID){
+    //Show one event
+    showGuests(eventID)
+    showAdmins(eventID)
+}
+function showGuests(eventID){
+    //Show event guests
+}
+function showAdmins(eventID){
+    //Show event admins
+}
