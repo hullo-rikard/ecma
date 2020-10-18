d1ad8a60cbc482c50d6c65309d16a4d253fda65e
rikardbjerkenius <rikard@hullo.se>
2020-10-18 15:44:57 +0200

    styling + await functions

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', async function(){
+document.addEventListener('DOMContentLoaded', async () => {
 
     const eventListDiv = document.querySelector('.eventList')
     const guestsListDiv = document.querySelector('.guestsList')
@@ -8,38 +8,53 @@ document.addEventListener('DOMContentLoaded', async function(){
     let guests = []
     let admins = []
 
+    //TODO: check if localstorage is used
+
     let events = await fetchEvents()
 
     showEvents(events, eventListDiv)
 
+
+
+
+    eventListDiv.addEventListener('click', (event) => {
+        if(event.target.getAttribute('eventid')){
+            console.log('EventID: '+event.target.getAttribute('eventid')+' clicked!')
+        }
+    })
 })
 
+
 async function fetchEvents() {
-    await fetch("../data/events.json")
+    let data = await fetch("../data/events.json")
     .then(response => response.json())
     .then(json => {
-        console.log(json.events)
         return json.events
-    });
+    })
+    return data
 }
 
-function showEvents(events, outputElement){
-    console.log(events, outputElement)
+function showEvents(events, outputDiv){
+    console.log(events)
     events.forEach(event => {
         let div =  document.createElement('div')
-        div.textContent = event.name
-        outputElement.appendChild(div)
+            div.setAttribute('eventid', event.id)
+            div.textContent = event.name
+        outputDiv.appendChild(div)
     })
 }
 
 function showEvent(eventID){
+    console.log('showEvent() called!')
     //Show one event
     showGuests(eventID)
     showAdmins(eventID)
 }
 function showGuests(eventID){
+    console.log('showGuests() called!')
     //Show event guests
 }
 function showAdmins(eventID){
+    console.log('showAdmins() called!')
     //Show event admins
 }
