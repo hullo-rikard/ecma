b0f923d461dfd99e762f51dcbcb769ec776f41ac
rikardbjerkenius <rikard@hullo.se>
2020-10-20 10:30:26 +0200

    wip

--- js/admin.js
+++ js/admin.js
@@ -1,35 +1,53 @@
-//this function should maybe be in the main.js file
-function getEventByEventID(eventsArray, ID){
-    for (var i=0; i < eventsArray.length; i++) {
-        if (eventsArray[i].id === ID) {
-            return eventsArray[i];
-        }
+function isEditable(boolean){
+    clearGuestsAndAdmins()
+    if(boolean){
+        document.querySelector('.createEvent').innerHTML = "Ändra event"
+        document.querySelector('form button').innerHTML = "Spara ändringar!"
+    } else {
+        document.querySelector('.createEvent').innerHTML = "Skapa nytt event<br><span>(eller klicka på ett event till vänster för att ändra)</span>"
+        document.querySelector('form button').innerHTML = "Skapa event!"
+        document.querySelector('#eventID').value = ""
+        let allInputs = document.querySelectorAll('input:not([type=file]), select, textarea')
+        allInputs.forEach(element => {
+            element.value = ""
+        })
     }
+}
+function clearGuestsAndAdmins(){
+    document.querySelector('.guestsList').innerHTML = ""
+    document.querySelector('.adminsList').innerHTML = ""
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
 
+    const ui = {
+        eventListDiv: document.querySelector('.eventList'),
+        guestsListDiv: document.querySelector('.guestsList'),
+        adminsListDiv: document.querySelector('.adminsList'),
+        newEventBtn: document.querySelector('.newEvent'),
+    }
+
     const eventListDiv = document.querySelector('.eventList')
     const guestsListDiv = document.querySelector('.guestsList')
     const adminsListDiv = document.querySelector('.adminsList')
-
-    let now = new Date()
-    let guests = []
-    let admins = []
+    const newEventBtn = document.querySelector('.newEvent')
 
     //TODO: check if localstorage is used
 
     let events = await fetchEvents()
 
-    showEvents(events, eventListDiv)
+    showEvents(events, ui)
 
     eventListDiv.addEventListener('click', (event) => {
         if(event.target.getAttribute('eventid')){
-            let thisevent = getEventByEventID(events, parseInt(event.target.getAttribute('eventid')) )
-            fillInputs(thisevent)
+            let thisevent = getEventByEventID(events, parseInt(event.target.getAttribute('eventid')))
+            showEvent(thisevent, ui)
+            console.log(thisevent)
         }
     })
+    newEventBtn.addEventListener('click', () => { isEditable(false) })
 })
+
 
 
 async function fetchEvents() {
@@ -41,35 +59,77 @@ async function fetchEvents() {
     return data
 }
 
-function showEvents(events, outputDiv){
-    console.log(events)
+function showEvents(events, ui){
     events.forEach(event => {
         let div =  document.createElement('div')
             div.setAttribute('eventid', event.id)
             div.textContent = event.name
-        outputDiv.appendChild(div)
+        ui.eventListDiv.appendChild(div)
     })
 }
 
-function showEvent(eventID){
-    console.log('showEvent() called!')
+function showEvent(thisevent, ui){
     //Show one event
-    showGuests(eventID)
-    showAdmins(eventID)
+    fillForm(thisevent, ui)
+    showGuests(thisevent, ui)
+    showAdmins(thisevent, ui)
 }
-function showGuests(eventID){
-    console.log('showGuests() called!')
-    //Show event guests
+function showGuests(thisevent, ui){
+    thisevent.members.guests.forEach((guest, key) => {
+        let div =  document.createElement('div')
+            div.setAttribute('guestid', key)
+            div.setAttribute('alt', guest.email)
+            div.setAttribute('title', guest.email)
+            div.textContent = guest.name
+            ui.guestsListDiv.appendChild(div)
+    })
 }
-function showAdmins(eventID){
-    console.log('showAdmins() called!')
-    //Show event admins
+function showAdmins(thisevent, ui){
+    thisevent.members.admins.forEach((admin, key) => {
+        let div =  document.createElement('div')
+            div.setAttribute('adminid', key)
+            div.setAttribute('alt', admin.password)
+            div.setAttribute('title', admin.password)
+            div.textContent = admin.username
+            ui.adminsListDiv.appendChild(div)
+    })
 }
-
-function fillInputs(thisevent) {
+function fillForm(thisevent, ui) {
+    isEditable(true)
     let allInputs = document.querySelectorAll('input:not([type=file]), select, textarea')
     allInputs.forEach(element => {
         element.value = thisevent[element.name]
     })
     //TODO: handle image
+}
+
+
+
+//this function should maybe be in the main.js file
+function getEventByEventID(eventsArray, ID){
+    for (var i=0; i < eventsArray.length; i++) {
+        if (eventsArray[i].id === ID) {
+            return eventsArray[i];
+        }
+    }
+}
+
+class Event {
+    constructor(event) {
+        this.id = event.id;
+        this.name = event.name;
+        this.info = event.description;
+        this.address = event.address;
+        this.venue = event.venue;
+        this.startDatetime = event.startDatetime;
+        this.endDatetime = event.endDatetime;
+        this.tickets = event.tickets;
+        this.category = event.category;
+        this.image = event.image;
+        this.displayingImg = event.image;
+        this.images = event.images;
+        this.admins = event.members.admins;
+        this.guests = event.members.guests;
+        this.guestbook = event.guestbook;
+    }
 }
