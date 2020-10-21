52b365f7d80ac3e88bf3504fbeefecc959ffa810
Dino Pacariz <dinopacariz@gmail.com>
2020-10-21 16:37:29 +0200

    "class:ified" version
    also changed default image to event-image link

--- js/events.js
+++ js/events.js
@@ -1,11 +1,148 @@
-let filteredCategories = []
 
+class Eventlist {
+    constructor(eventlist) {
+        this.events = eventlist
+        this.filter = {
+            memory: [],
+            add(input) {
+                this.memory.push(input)
+            },
+            remove(input) {
+                this.memory.splice(this.memory.indexOf(input), 1)
+            }
+        }
+        this.div = {
+            upcoming: document.getElementById('upcomingEvents'),
+            past: document.getElementById('pastEvents'),
+            filter: document.getElementById('filter'),
+            sort: document.getElementById('sort')
+        }
+        this.updateEvents()
+        this.eventListeners()
+    }
 
+    updateEvents() {
+        this.removeEvents(this.div.upcoming, this.div.past)
+        this.addEvents(this.sortEvents(this.events))
+    }
+
+    sortEvents(events) {
+        let dateRadio = document.getElementById('date')
+        
+        if (dateRadio.checked == true) {            
+            events.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
+        }
+        else {
+            events.sort(function(a, b){return a.name.localeCompare(b.name)});
+        }
+        return events
+    }
+
+    removeEvents(upcoming, past) {
+        while (upcoming.childNodes.length) {
+            upcoming.removeChild(upcoming.lastChild);
+        }
+        while (past.childNodes.length) {
+            past.removeChild(past.lastChild);
+        }
+    }
+
+    addEvents(events) {
+        let now = new Date()
+    
+        for (let event of events) {
+            let eventDate = new Date(event.startDatetime)
+    
+            if (eventDate > now) {
+                this.div.upcoming.appendChild(assembleEventNode(event, this))
+            } else this.div.past.appendChild(assembleEventNode(event, this))
+    
+            function assembleEventNode(event, self) {
+                let eventLink = Object.assign(document.createElement('a'),{href:'event.html?eventid=' + event.id})
+                let outerDiv = Object.assign(document.createElement('div'),{classList:'event'})
+                outerDiv.setAttribute('category', event.category)
+        
+                let imageDiv = Object.assign(document.createElement('div'),{classList:'thumbnail'})
+                let image = Object.assign(document.createElement('img'),{src: event.image})
+                imageDiv.appendChild(image)
+        
+                let infoDiv = document.createElement('div')
+                let title = Object.assign(document.createElement('h2'),{innerHTML: event.name})
+                let address = Object.assign(document.createElement('p'),{innerHTML: event.address})
+                let date = Object.assign(document.createElement('h3'),{innerHTML: event.startDatetime.replace("T", ", Kl ").slice(0, -3)})
+        
+                infoDiv.appendChild(title)
+                infoDiv.appendChild(date)
+                infoDiv.appendChild(address)
+                
+                outerDiv.appendChild(imageDiv)
+                outerDiv.appendChild(infoDiv)
+                
+                if (self.filter.memory.includes(event.category)){
+                    outerDiv.classList.add('hidden')
+                }
+    
+                eventLink.appendChild(outerDiv)
+    
+                return eventLink
+            }
+        }
+    }
+
+    eventListeners() {
+        let eventDivs = document.getElementsByClassName('event')
+        
+        this.div.sort.addEventListener('change', () => {
+            this.updateEvents()
+        })
+    
+        this.div.filter.addEventListener('click', (e) => {
+            if (!isCategory()) {
+                return
+            }
+            
+            if (!isFiltered()) {
+                this.filter.add(e.target.id)
+                e.target.classList.add('filtered')
+    
+                for (let event of eventDivs) {
+                    hideCategoryEvent(event)
+                }
+            }
+            else {
+                this.filter.remove(e.target.id)
+                e.target.classList.remove('filtered')
+                
+                for (let event of eventDivs) {
+                    unhideCategoryEvent(event)
+                }
+            }
+            function isCategory() {
+                if(e.target.classList.contains('filterChoice')) {
+                    return true
+                }
+            }
+            function isFiltered() {
+                if(e.target.classList.contains('filtered')) {
+                    return true
+                }
+            }
+            function hideCategoryEvent(event) {
+                if(event.getAttribute('category') == e.target.id) {
+                    event.classList.add('hidden')
+                }
+            }
+            function unhideCategoryEvent(event) {
+                if(event.getAttribute('category') == e.target.id) {
+                    event.classList.remove('hidden')
+                }
+            }
+        })
+    }
+}
 
 document.addEventListener('DOMContentLoaded', async function(){  
-    let events = await fetchEvents()
-    updateEvents(events)
-    eventListeners(events)
+    let myevents = new Eventlist(await fetchEvents())
 })
 
 async function fetchEvents() {
@@ -14,131 +151,4 @@ async function fetchEvents() {
     .then(response => response.json())
     .then(data => {return data})
     return fetched.events
-}
-
-function updateEvents(events) {
-    removeEvents()
-    addEvents(sortEvents(events))
-}
-
-function sortEvents(events) {
-    let dateRadio = document.getElementById('date')
-    
-    if (dateRadio.checked == true) {            
-        events.sort(function(a, b){return new Date(a.startDatetime) - new Date(b.startDatetime)});
-    }
-    else {
-        events.sort(function(a, b){return a.name.localeCompare(b.name)});
-    }
-    return events
-}
-
-function removeEvents() {
-    let upcomingEventsDiv = document.getElementById('upcomingEvents')
-    let pastEventsDiv = document.getElementById('pastEvents')
-
-    while (upcomingEventsDiv.childNodes.length) {
-        upcomingEventsDiv.removeChild(upcomingEventsDiv.lastChild);
-    }
-    while (pastEventsDiv.childNodes.length) {
-        pastEventsDiv.removeChild(pastEventsDiv.lastChild);
-    }
-}
-
-function addEvents(events) {
-    let upcomingEventsDiv = document.getElementById('upcomingEvents')
-    let pastEventsDiv = document.getElementById('pastEvents')
-    let now = new Date()
-
-    for (let event of events) {
-        let eventDate = new Date(event.startDatetime)
-
-        if (eventDate > now) {
-            upcomingEventsDiv.appendChild(assembleEventNode(event))
-        } else pastEventsDiv.appendChild(assembleEventNode(event))
-
-        function assembleEventNode(event) {
-            console.log(event)
-            let eventLink = Object.assign(document.createElement('a'),{href:'event.html?eventid=' + event.id})
-            let outerDiv = Object.assign(document.createElement('div'),{classList:'event'})
-            outerDiv.setAttribute('category', event.category)
-    
-            let imageDiv = Object.assign(document.createElement('div'),{classList:'thumbnail'})
-            let image = Object.assign(document.createElement('img'),{src:'../img/defaultEvent.jpg'})
-            imageDiv.appendChild(image)
-    
-            let infoDiv = document.createElement('div')
-            let title = Object.assign(document.createElement('h2'),{innerHTML: event.name})
-            let address = Object.assign(document.createElement('p'),{innerHTML: event.address})
-            let date = Object.assign(document.createElement('h3'),{innerHTML: event.startDatetime.replace("T", ", Kl ").slice(0, -3)})
-    
-            infoDiv.appendChild(title)
-            infoDiv.appendChild(date)
-            infoDiv.appendChild(address)
-            
-            outerDiv.appendChild(imageDiv)
-            outerDiv.appendChild(infoDiv)
-            
-            if (filteredCategories.includes(event.category)){
-                outerDiv.classList.add('hidden')
-            }
-
-            eventLink.appendChild(outerDiv)
-
-            return eventLink
-        }
-    }
-}
-
-function eventListeners(events) {
-    let eventDivs = document.getElementsByClassName('event')
-    let filterDiv = document.getElementById('filter')
-    let sortDiv = document.getElementById('sort')
-    
-    sortDiv.addEventListener('change', function(){
-        updateEvents(events)
-    })
-
-    filterDiv.addEventListener('click', function(e){
-        if (!isCategory()) {
-            return
-        }
-        
-        if (!isFiltered()) {
-            filteredCategories.push(e.target.id)
-            e.target.classList.add('filtered')
-
-            for (let event of eventDivs) {
-                hideCategoryEvent(event)
-            }
-        }
-        else {
-            filteredCategories.splice(filteredCategories.indexOf(e.target.id), 1)
-            e.target.classList.remove('filtered')
-            
-            for (let event of eventDivs) {
-                unhideCategoryEvent(event)
-            }
-        }
-        function isCategory() {
-            if(e.target.classList.contains('filterChoice')) {
-                return true
-            }
-        }
-        function isFiltered() {
-            if(e.target.classList.contains('filtered')) {
-                return true
-            }
-        }
-        function hideCategoryEvent(event) {
-            if(event.getAttribute('category') == e.target.id) {
-                event.classList.add('hidden')
-            }
-        }
-        function unhideCategoryEvent(event) {
-            if(event.getAttribute('category') == e.target.id) {
-                event.classList.remove('hidden')
-            }
-        }
-    })
 }
