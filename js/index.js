30269fddeabe3f5c838cd707a6ca66c456e6835d
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-28 10:44:35 +0100

    added comments in index.js and index.html

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", async function(e){
+
+    //----------Hämtar data från local storage via klassen App i main.js-----------//
     
     eventData = new App(JSON.parse(localStorage.getItem('events')));
-    console.log(eventData);
+
+    //----------Ger räknaren ett initialvärde och kallar på cykel-funktionen-----------//
 
     counter = 0;
     cycleDisplay();
 
 })
+
+//----------Funktionen som byter ut text och bilder-----------//
 
 function cycleDisplay(){ 
 
@@ -14,7 +19,7 @@ function cycleDisplay(){
     if (counter == eventData.events.length){
         counter = 0; 
     }
-    
+
     document.getElementById("display").style.backgroundImage = "url(" + eventData.events[counter].image + ")";
 
     document.getElementById("event_name").innerHTML = eventData.events[counter].name;
@@ -26,6 +31,8 @@ function cycleDisplay(){
     document.getElementById("event_tickets").innerHTML = eventData.events[counter].tickets + " biljetter kvar.";
 
     document.getElementById("to_event").href = "event.html?eventid=" + eventData.events[counter].id;
+
+    //----------Timer som kallar om funktionen var 5000:e milisekund-----------//
 
     setTimeout(cycleDisplay, 5000);
 }
