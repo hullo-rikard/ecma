73a2646de126739d9432461bb6403c6e257b27b3
Daniel Koefoed <dc.koefoed@gmail.com>
2020-10-28 10:12:51 +0100

    fixed the cycle-counter to events-array length, removed unused code

--- js/index.js
+++ js/index.js
@@ -3,38 +3,29 @@ document.addEventListener("DOMContentLoaded", async function(e){
     eventData = new App(JSON.parse(localStorage.getItem('events')));
     console.log(eventData);
 
-
-    cycle = new Cycle(5, 5);
+    counter = 0;
     cycleDisplay();
 
 })
 
 function cycleDisplay(){ 
 
-    cycle.i++    
-    if (cycle.i > cycle.cycleMax){
-        cycle.i = (cycle.cycleMax - cycle.cycleRange); 
+    counter++    
+    if (counter == eventData.events.length){
+        counter = 0; 
     }
     
-    document.getElementById("display").style.backgroundImage = "url(" + eventData.events[cycle.i].image + ")";
+    document.getElementById("display").style.backgroundImage = "url(" + eventData.events[counter].image + ")";
 
-    document.getElementById("event_name").innerHTML = eventData.events[cycle.i].name;
+    document.getElementById("event_name").innerHTML = eventData.events[counter].name;
 
-    document.getElementById("event_startdate").innerHTML = eventData.events[cycle.i].startDatetime.replace("T", ", Kl ").slice(0, -3); 
+    document.getElementById("event_startdate").innerHTML = eventData.events[counter].startDatetime.replace("T", ", Kl ").slice(0, -3); 
 
-    document.getElementById("event_location").innerHTML = eventData.events[cycle.i].venue + ",<br>" + eventData.events[cycle.i].address;
+    document.getElementById("event_location").innerHTML = eventData.events[counter].venue + ",<br>" + eventData.events[counter].address;
 
-    document.getElementById("event_tickets").innerHTML = eventData.events[cycle.i].tickets + " biljetter kvar.";
+    document.getElementById("event_tickets").innerHTML = eventData.events[counter].tickets + " biljetter kvar.";
 
-    document.getElementById("to_event").href = "event.html?eventid=" + eventData.events[cycle.i].id;
+    document.getElementById("to_event").href = "event.html?eventid=" + eventData.events[counter].id;
 
     setTimeout(cycleDisplay, 5000);
-}
-
-class Cycle{
-    constructor(cycleRange, cycleMax){
-        this.cycleRange = cycleRange;
-        this.cycleMax = cycleMax;
-        this.i = (this.cycleMax - this.cycleRange);
-    }
 }
